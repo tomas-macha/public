0ddc849d2eafa96859b521e2d5ffdb25c8dee65e
Tomáš Mácha <112073439+tomas-macha@users.noreply.github.com>
2023-03-30 18:08:33 +0200

    Update main.js
    Zrychlení načítání a zátěže (přidán lazyload pro obrázky a iframe).

--- stredocesky-kraj/main.js
+++ stredocesky-kraj/main.js
@@ -15,7 +15,26 @@ const showSlide = async s => {
 	}
 	od.style.display = "none";
 	nw.style.display = "block";
-};
+	loadData(slide);
+	loadData(slide+1);
+	loadData(slide+2);
+	loadData(slide+3);
+}
+
+function loadData(idx){
+	const ifr = slides[idx].getElementsByTagName("IFRAME");
+	for(const i of ifr){
+		if(i.hasAttribute("lazy-src")){
+			const val = i.getAttribute("lazy-src");
+			if(i.src != val)i.src = val;
+		}
+	}
+	const img = slides[idx].getElementsByClassName("bgimg");
+	for(const i of img){
+		const val = `url(${i.getAttribute("src")})`;
+		if(i.style.backgroundImage != val)i.style.backgroundImage = val;
+	}
+}
 
 const init = ()=>{
 	slides.forEach(s => {
@@ -34,7 +53,7 @@ const init = ()=>{
 		}
 	});
 	document.querySelectorAll(".bgimg").forEach(e => {
-		e.style.backgroundImage = `url(${e.getAttribute("src")})`;
+		//e.style.backgroundImage = `url(${e.getAttribute("src")})`;
 	});
 	start();
 };
